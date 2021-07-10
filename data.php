a37e0c0a77d75c59f99b9a9a159f4c422c06396f
❎❎itsneutral❎❎ <77259546+neutralarmy@users.noreply.github.com>
2021-07-10 11:54:53 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+//Enter your username and Password Here
+$username = "neutralarmy";
+$pass = "Shashank2708";
+
+
+// Enter Useragent here
+$useragent = "Mozilla/5.0 (Linux; Android 7.1.2; SM-J320F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Mobile Safari/537.36";
+
+// Enter autologin Cred. here
+$autologin = "ses_user%3Dneutralarmy%26ses_hash%3D3094e99410eebdd9f8404b37df5731e1";
+
+// Enter Phessid Here
+$ssid = "og1efol52ukoiq0l1u4ie55mt5";
+
+
+
