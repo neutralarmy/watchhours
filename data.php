ff1f730ad10ded7f9352fb48216ce76f2fecaee1
❎❎itsneutral❎❎ <77259546+neutralarmy@users.noreply.github.com>
2021-07-10 11:57:25 +0530

    Update data.php

--- data.php
+++ data.php
@@ -3,18 +3,18 @@
 
 
 //Enter your username and Password Here
-$username = "neutralarmy";
-$pass = "Shashank2708";
+$username = "xxxxxx";
+$pass = "xxxxxxxx";
 
 
 // Enter Useragent here
-$useragent = "Mozilla/5.0 (Linux; Android 7.1.2; SM-J320F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Mobile Safari/537.36";
+$useragent = "xxxxxxxxxxxxx";
 
 // Enter autologin Cred. here
-$autologin = "ses_user%3Dneutralarmy%26ses_hash%3D3094e99410eebdd9f8404b37df5731e1";
+$autologin = "xxxxxxxxxxxxxxxxxxxxxxxx";
 
 // Enter Phessid Here
-$ssid = "og1efol52ukoiq0l1u4ie55mt5";
+$ssid = "xxxxxxxxxxxxxxxxxxxx";
 
 
 
